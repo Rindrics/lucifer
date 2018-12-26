b7ac79df0b2d4ca39c87e825e98b40022877c5f8
Akira Hayashi <smxshxishxad@me.com>
2018-12-26 18:08:55 +0900

    Add test on parsing date

--- reformr/tests/testthat/test_filestring.R
+++ reformr/tests/testthat/test_filestring.R
@@ -26,9 +26,17 @@ test_that("get_filelist() puts species file list correctly", {
 
 infile06 <- "/Users/ahayashi/Documents/GitHub/tidyNAS/data/鮮魚関係/鮮魚測定06/カタクチイワシ.xls"
 infile17 <- "/Users/ahayashi/Documents/GitHub/tidyNAS/data/鮮魚関係/鮮魚測定17/カタクチイワシ.xls"
+
 test_that("get_sheet2read() puts sheets to read", {
   expect_match(get_sheet2read(infile06), "0125", all = FALSE)
   expect_match(get_sheet2read(infile06), "0207", all = FALSE)
   expect_equal(length(get_sheet2read(infile06)), 10)
   expect_equal(length(get_sheet2read(infile17)), 0)
+})
+
+test_that("get_date() parses dates correctly", {
+  expect_equal(get_date(2012, "0201"), as.Date("2012-02-01"))
+  expect_equal(get_date(2012, "0201(1)"), as.Date("2012-02-01"))
+  expect_equal(get_date(2017, "170201(1)"), as.Date("2017-02-01"))
+  expect_equal(get_date(2017, "170201(11)"), as.Date("2017-02-01"))
 })
