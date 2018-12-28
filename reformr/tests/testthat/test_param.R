6e8726134bd98b9d5f8c2fb079ff73643c76cda1
Akira Hayashi <smxshxishxad@me.com>
2018-12-28 17:09:18 +0900

    Add spcs argument to make_params()

--- reformr/tests/testthat/test_param.R
+++ reformr/tests/testthat/test_param.R
@@ -4,3 +4,7 @@ test_that("make_params accept only 'sengyo' or 'cruise'", {
   expect_error(make_params(indir = "./", "a"), "Tell me the correct type of data. Is it 'sengyo', or 'cruise?'", fixed = TRUE)
   expect_error(make_params(indir = "./", 1), "Tell me the correct type of data. Is it 'sengyo', or 'cruise?'", fixed = TRUE)
 })
+test_that("make_params accept only 'カタクチイワシ', 'マイワシ' or 'マアジ' ", {
+  expect_error(make_params(indir = "./", "sengyo", 1), "Tell me the correct Japanese species name.", fixed = TRUE)
+  expect_error(make_params(indir = "./", "sengyo", "Anchovy"),  "Tell me the correct Japanese species name.", fixed = TRUE)
+})
