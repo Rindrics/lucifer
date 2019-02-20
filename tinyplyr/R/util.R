cc6318cb86575b2b008ce291fdbbb95d607c357d
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-02-20 11:39:52 +0900

    Update by tangle

--- tinyplyr/R/util.R
+++ tinyplyr/R/util.R
@@ -4,7 +4,7 @@
 # Do not edit by hand.                                                        |
 # ---------------------------------------------------------------------------'
 
-num2date <- function(x) {
+num2datei <- function(x) {
   jday_march01 <- 60
   x            <- as.numeric(x)
   if (x == jday_march01) {
@@ -15,4 +15,56 @@ num2date <- function(x) {
   }
   x <- x - 1 # because of origin
   date <- as.character(as.Date(x, origin = "1900-01-01"))
+}
+
+num2date <- function(x) {
+  out <- purrr::map(x, num2datei)
+  unlist(out)
+}
+
+is.jpdate <- function(x) {
+  stringr::str_detect(x, "[A-Z]\\.?[0-9]+\\..")
+}
+
+split_jpdate <- function(x) {
+  initial <- substr(x, 1, 1)
+  switch(initial,
+       "H" = era <- "heisei")
+
+  year  <- stringr::str_match(x, "^[A-Za-z]\\.?([0-9]+)\\.")[2]
+  month <- stringr::str_match(x, "^[A-Za-z]\\.?[0-9]+\\.([0-9]+)")[2]
+  day   <- stringr::str_match(x, "^[A-Za-z]\\.?[0-9]+\\.[0-9]+\\.([0-9]+)")[2]
+  out   <- list("era"   = era,
+                "year"  = as.numeric(year),
+                "month" = as.numeric(month),
+                "day"   = as.numeric(day)
+                )
+  out
+}
+split_jpdate("H20.4.30")
+
+jpdate2juliani <- function(x) {
+  if (is.jpdate(x) == TRUE) {
+    split  <- split_jpdate(x)
+    era    <- split$era
+    jpyear <- stringr::str_match(x, "[A-Z]")
+    switch(era,
+           "heisei" = {
+             year  <- split$year + 1988
+             month <- split$month
+             day   <- split$day
+             date  <- as.Date(paste(year, month, day, sep = "-"))
+           })
+    jday <- julian.Date(date, origin = as.Date("1900-01-01"))
+    jday <- jday + 1 # Correct origin difference
+    jday <- jday + 1 # Correct leap year bug
+  } else {
+    jday <- x
+  }
+  jday[1]
+}
+
+jpdate2julian <- function(x) {
+  out <- purrr::map(x, jpdate2juliani)
+  unlist(out)
 }
--- tinyplyr/tests/testthat/test_util.R
+++ tinyplyr/tests/testthat/test_util.R
@@ -3,12 +3,31 @@
 # (https://github.com/smxshxishxad/tidyNAS/blob/master/tinyplyr.org) |
 # Do not edit by hand.                                               |
 # ------------------------------------------------------------------'
-context("Util functions work correctly")
+context("Parse strings correctly")
 
-test_that("num2date () convert numdate from Excel correctly", {
-  expect_equal(num2date(58), "1900-02-27")
-  expect_equal(num2date(59), "1900-02-28")
-  expect_error(num2date(60), "This date is not correct in Excel.")
-  expect_equal(num2date(61), "1900-03-01")
-  expect_equal(num2date(62), "1900-03-02")
+test_that("num2datei () convert numdate from Excel correctly", {
+  expect_equal(num2datei(58), "1900-02-27")
+  expect_equal(num2datei(59), "1900-02-28")
+  expect_error(num2datei(60), "This date is not correct in Excel.")
+  expect_equal(num2datei(61), "1900-03-01")
+  expect_equal(num2datei(62), "1900-03-02")
+})
+
+test_that("is.jpdate() judge if given str is a jpdate", {
+  expect_true(is.jpdate("H.29.8.22"))
+  expect_true(is.jpdate("H29.8.22"))
+  expect_false(is.jpdate("2000.8.22"))
+})
+
+test_that("split_jpdate() returns factors of jpdate", {
+  split <- split_jpdate("H.29.08.22")
+  expect_is(split, "list")
+  expect_equal(split$year, 29)
+  expect_equal(split$month, 8)
+  expect_equal(split$day, 22)
+})
+
+test_that("jpdate2juliani() convert Japanese date to Julian day", {
+  expect_equal(jpdate2juliani("H.29.8.22"), 42969)
+  expect_equal(jpdate2juliani("H29.8.22"), 42969)
 })
