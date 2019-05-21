3725a01c82961e6c5c1d1c0e656ed5903af2bd0c
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-05-21 09:27:27 +0900

    Add %<>%

--- R/util.R
+++ R/util.R
@@ -6,4 +6,11 @@
 #' @export
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
+#'
+#' #' @name %<>%
+#' @rdname doublepipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
 NULL
