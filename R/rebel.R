e465c84cddadfe48f59ebe60490e9d95a20efc9f
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-06-03 16:32:11 +0900

    :sparkles: Bundle extract_cluster() in rebel_sheet()

--- R/rebel.R
+++ R/rebel.R
@@ -3,15 +3,37 @@
 #' @inheritParams readxl::read_excel
 #' @param row_merged Row position of merged colnames
 #' @param col_merged Column position of merged rownames
-rebel_sheet <- function(sheet, path, row_merged, col_merged) {
+#' @param cluster List of cluster, direction, pos
+rebel_sheet <- function(sheet, path, row_merged = 0, col_merged = 0,
+                        cluster = NULL) {
   path <- structure(path,
                     sheet = sheet,
                     row_merged = row_merged,
-                    col_merged = col_merged)
+                    col_merged = col_merged,
+                    cluster = cluster)
   path
   attributes <- attributes(path)
 
   out <- load_alldata(path, sheet = sheet)
+
+  if (!is.null(cluster)) {
+    dir    <- cluster$dir
+    pos    <- cluster$pos
+    regex  <- cluster$regex
+    offset <- cluster$offset
+    dim    <- cluster$dim
+    if (dir == "row") {
+      out <- extract_clusters(df = out, regex = regex, col = pos,
+                            offset = offset, dim = dim)
+    } else if (dir == "col") {
+      out <- extract_clusters(df = out, regex = regex, row = pos,
+                            offset = offset, dim = dim)
+    } else {
+      stop("Unknown dir")
+    }
+  out
+  }
+
   if (attributes$row_merged > 0) {
     out <- unmerge_vert(out, col = attributes$row_merged)
   }
@@ -20,17 +42,26 @@ rebel_sheet <- function(sheet, path, row_merged, col_merged) {
     out <- unmerge_horiz(out, row = attributes$col_merged) %>%
       merge_colname(rows = 1:(attributes$col_merged + 1))
   }
-  out <- headerize(out, row = 1)
+  if (is.list(out) & is.null(dim(out))) {
+    out <- out %>%
+      lapply(headerize, row = 1) %>%
+      purrr::invoke(rbind, .)
+  } else {
+    out <- headerize(as.data.frame(out), row = 1) %>% tibble::as_tibble()
+  }
 }
 
 #' Rebel against godly Excel workbook
 #'
 #' @inheritParams rebel_sheet
 #' @param sheet.regex Regular expression to match sheetname
-rebel <- function(path, sheet.regex, row_merged = 0, col_merged = 0) {
+rebel <- function(path, sheet.regex, row_merged = 0, col_merged = 0,
+                  cluster = NULL) {
   sheets <- stringr::str_extract(readxl::excel_sheets(path), sheet.regex)
   out <- purrr::map_df(sheets, rebel_sheet, path = path,
              row_merged = row_merged,
-             col_merged = col_merged)
+             col_merged = col_merged,
+             cluster = cluster)
+  out
   as.data.frame(out)
 }
--- tests/testthat/test_rebel.R
+++ tests/testthat/test_rebel.R
@@ -10,6 +10,32 @@ test_that("rebel_sheet() beat up file with merged header", {
                c("A2", paste0(LETTERS[c(1, 3:8)], 3)))
 })
 
+test_that("rebel_sheet() beat up file with blocked data", {
+  beaten <- rebel_sheet(path = "clustered.xlsx", sheet = "foo",
+                        cluster = list(dir = "col",
+                                       pos = 1,
+                                       regex = "...",
+                                       offset = c(1, 0),
+                                       dim = c(3, 4)))
+  expect_equal(colnames(beaten), c("this", "is", "a", "test"))
+  expect_equal(dplyr::pull(beaten, 1),
+               rep(c("A2", "A3"), 4))
+  expect_equal(dplyr::pull(beaten, 2),
+               as.character(c(2, 3, 12, 13, 22, 23, 32, 33)))
+  expect_equal(dplyr::pull(beaten, 4),
+               as.character(c(62, 63, 72, 73, 82, 83, 92, 93)))
+
+  beaten <- rebel_sheet(path = "clustered.xlsx", sheet = "foo",
+                  cluster = list(dir = "col",
+                                 pos = 1,
+                                 regex = "b..",
+                                 offset = c(1, 2),
+                                 dim = c(5, 2)))
+  expect_equal(colnames(beaten), c("a", "test"))
+  expect_equal(as.numeric(dplyr::pull(beaten, 1)), c(42:45, 52:55, 62:65))
+  expect_equal(as.numeric(dplyr::pull(beaten, 2)), c(72:75, 82:85, 92:95))
+})
+
 test_that("rebel() beat up file with merged header", {
   beaten <- rebel(path = "merged.xlsx", sheet.regex = "Sheet.",
                         row_merged = 1, col_merged = 1) %>%
