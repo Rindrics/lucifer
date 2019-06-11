4bfe9c3048a1258b82e55ffa6e18745790a0c43b
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-06-11 17:12:07 +0900

    :white_check_mark: Add test for load_alldata()

--- tests/testthat/test_load_data.R
+++ tests/testthat/test_load_data.R
@@ -9,4 +9,11 @@ test_that("load_alldata loads Excel file as text", {
   expect_equal(class(dplyr::pull(data, 4)), "character")
 })
 
-#load_rect
+test_that("load_alldata loads aomori data as text", {
+  data <- load_alldata(path = "aomori.xlsx", sheet = "マイワシ")
+  expect_is(data, "data.frame")
+  expect_equal(class(dplyr::pull(data, 1)), "character")
+  expect_equal(class(dplyr::pull(data, 2)), "character")
+  expect_equal(class(dplyr::pull(data, 3)), "character")
+  expect_equal(class(dplyr::pull(data, 4)), "character")
+})
