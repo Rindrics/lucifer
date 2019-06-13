153ebfecf1f3cc5151dee7414b38a40f560304cb
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-06-13 11:16:10 +0900

    :white_check_mark: Rebel aomori using rebel_sheet
    Beat 'katakuch' sheet up

--- tests/testthat/test_rebel_prefec.R
+++ tests/testthat/test_rebel_prefec.R
@@ -3,7 +3,7 @@ context("Rebel prefectures using rebel()")
 test_that("rebel_sheet() beat aomori data up", {
   fname <- "aomori.xlsx"
   sheet <- "マイワシ"
-  beaten <- rebel_sheet(path = fname, sheet = sheet,
+  maiwashi <- rebel_sheet(path = fname, sheet = sheet,
                         cluster = list(dir = "row",
                                        pos = 1,
                                        regex = "^年",
@@ -16,10 +16,31 @@ test_that("rebel_sheet() beat aomori data up", {
                         col_type = list(regex = "^1?[0-9]月?",
                                         newname = "month",
                                         varname = "catch"))
-  expect_setequal(beaten$catch, as.character(1:936))
-  expect_equal(unique(beaten$month), paste0(1:12, "月"))
-  expect_equal(unique(beaten$`年／月`), as.character(1981:2019))
-  expect_equal(unique(beaten$漁法), c("まき網漁業", "定置網漁業（底建網含む）"))
-  expect_equal(unique(beaten$fname), fname)
-  expect_equal(unique(beaten$sheet), sheet)
+  expect_setequal(maiwashi$catch, as.character(1:936))
+  expect_equal(unique(maiwashi$month), paste0(1:12, "月"))
+  expect_equal(unique(maiwashi$`年／月`), as.character(1981:2019))
+  expect_equal(unique(maiwashi$漁法), c("まき網漁業", "定置網漁業（底建網含む）"))
+  expect_equal(unique(maiwashi$fname), fname)
+  expect_equal(unique(maiwashi$sheet), sheet)
+
+  sheet <- "カタクチ"
+  katakuchi <- rebel_sheet(path = fname, sheet = sheet,
+                        cluster = list(dir = "row",
+                                       pos = 1,
+                                       regex = "^年",
+                                       offset = c(0, 0),
+                                       dim = c(40, 13),
+                                       info = list(offset = c(-2, 0),
+                                                   dim = c(1, 2),
+                                                   headerized = FALSE)),
+                        row_type = "Y",
+                        col_type = list(regex = "^1?[0-9]月?",
+                                        newname = "month",
+                                        varname = "catch"))
+  expect_setequal(katakuchi$catch, as.character(1:936))
+  expect_equal(unique(katakuchi$month), paste0(1:12, "月"))
+  expect_equal(unique(katakuchi$`年／月`), as.character(1981:2019))
+  expect_equal(unique(katakuchi$漁法), c("まき網漁業", "定置網漁業（底建網含む）"))
+  expect_equal(unique(katakuchi$fname), fname)
+  expect_equal(unique(katakuchi$sheet), sheet)
 })
