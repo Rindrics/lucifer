0ca73354b5b8ea131615a40857ad9e0611968b01
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-06-13 10:59:49 +0900

    :white_check_mark: Fight with aomori data with bultitools
    Fight with
    - 'katakuchi' sheet
    - 'masabahi' sheet

--- tests/testthat/test_multitools.R
+++ tests/testthat/test_multitools.R
@@ -1,6 +1,6 @@
 context("Fights using multitools")
 
-test_that("Fight with aomori using multitools", {
+test_that("Fight with maiwashi sheet of aomori data using multitools", {
   df <- load_alldata("aomori.xlsx", sheet = "マイワシ")
   converted <- df %>%
     extract_clusters(regex = "^年", col = 1,
@@ -16,4 +16,46 @@ test_that("Fight with aomori using multitools", {
   expect_equal(unique(converted$漁法), c("まき網漁業", "定置網漁業（底建網含む）"))
   expect_equal(unique(converted$month), paste0(1:12, "月"))
   expect_setequal(converted$catch, as.character(seq(1, 39 * 24)))
+})
+
+test_that("Fight with katakuchi sheet of aomori data using multitools", {
+  df <- load_alldata("aomori.xlsx", sheet = "カタクチ")
+  converted <- df %>%
+    extract_clusters(regex = "^年", col = 1,
+                     offset = c(0, 0), dim = c(40, 13),
+                     info = list(offset = c(-2, 0),
+                                 dim = c(1, 2))) %>%
+    lapply(headerize, row = 1) %>%
+    purrr::invoke(rbind, .) %>%
+    rm_nacols() %>%
+    gather_cols(regex = "1?[0-9]月",
+                newname = "month", varname = "catch")
+  expect_equal(colnames(converted), c("年／月", "漁法", "month", "catch"))
+  expect_equal(unique(converted$漁法), c("まき網漁業", "定置網漁業（底建網含む）"))
+  expect_equal(unique(converted$month), paste0(1:12, "月"))
+  expect_setequal(converted$catch, as.character(seq(1, 39 * 24)))
+})
+
+test_that("Fight with 'masabahi' sheet of aomori data using multitools", {
+  df <- load_alldata("aomori.xlsx", sheet = "マサバ比  ")
+
+  hachinohe <- df %>%
+    extract_clusters(regex = "八戸", col = 1,
+                     offset = c(1, 1), dim = c(9, 3)) %>%
+    headerize(1)
+  expect_equal(colnames(hachinohe), c("年月日", "マサバ", "ゴマサバ"))
+  expect_equal(dplyr::pull(hachinohe, 1),
+               as.character(c(43305, 43315, 43334, 43343,
+                              43383, 43410, 43431, 43452)))
+  expect_equal(dplyr::pull(hachinohe, 2), as.character(1:8))
+  expect_equal(dplyr::pull(hachinohe, 3), as.character(99:92))
+
+  tairadate <- df %>%
+    extract_clusters(regex = "平舘", col = 1,
+                     offset = c(1, 1), dim = c(15, 3)) %>%
+    headerize(1)
+  expect_equal(colnames(tairadate), c("月漁獲量", "マサバ", "ゴマサバ"))
+  expect_equal(dplyr::pull(tairadate, 1), as.character(c(1:12, 1, 2)))
+  expect_equal(dplyr::pull(tairadate, 2), as.character(1:14))
+  expect_equal(dplyr::pull(tairadate, 3), as.character(99:86))
 })
